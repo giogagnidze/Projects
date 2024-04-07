3129a206a483b93d9ed139de9d334c36f1b00775
giogagnidze <giogagnie@gmail.com>
2024-04-07 18:12:11 +0400

    commit

--- N/N.py
+++
@@ -1,2 +0,0 @@
-for i in range(10001):
-    print(str(i) + ". i love you")
---
+++ do you love me/script.js
@@ -0,0 +1,16 @@
+const buttonHeight = 50
+const buttonWidth = 150
+
+const maxWidth = window.innerWidth - buttonWidth;
+const maxHeight = window.innerHeight - buttonHeight;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+    const button1 = document.getElementById('button1')
+    button.addEventListener('click', () => alert('whyy'))
+    button1.addEventListener('click', () => alert('I Know Bebe <3'))
+    button.addEventListener('mouseover', () => {
+        button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+    })
+});
